cbcf69dfc2cbb465c03862e1f60b3ed28593e65d
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-29 17:51:11 +0530

    updating main urls

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings  
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  
+from django.contrib.staticfiles.urls import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
