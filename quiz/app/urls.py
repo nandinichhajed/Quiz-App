9f7255d162590e5ec6b0d4a638de25932d81a563
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-29 17:52:04 +0530

    adding views and urls of the app

---
+++ quiz/app/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings  
+from django.conf.urls.static import static  
+from django.urls import path, include  
+from . import views  
+app_name = "App"  
+
+urlpatterns = [  
+ path("", views.login, name="login"),  
+ path("login", views.login, name="login"),  
+ path("dashboard", views.dashboard, name="dashboard"),  
+ path("quiz", views.quiz, name="quiz"),  
+ path("register", views.register, name="register"),  
+ path("create-quiz", views.create_quiz, name="create-quiz"),  
+ path("create-questions", views.create_question, name="create-question"),  
+ path("answer-quiz/<slug>", views.answer_quiz, name="answer-quiz"),  
+]
--- quiz/app/views.py
+++ quiz/app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect  
+from django.contrib import messages  
+from django.core.paginator import Paginator  
+from django.http import HttpResponseNotFound  
+from faunadb import query as q  
+import pytz  
+from faunadb.objects import Ref  
+from faunadb.client import FaunaClient  
+import hashlib  
+import datetime  
 
-# Create your views here.
+client = FaunaClient(secret="fnAEZMXmQ9AAQAT9sh4e06BXOx8quvVauMz7X2Qt")  
+indexes = client.query(q.paginate(q.indexes()))
